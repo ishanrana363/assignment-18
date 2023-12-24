54725c52fb79d2df3d46f51d0431ccc68077151e
ishanrana363 <ishanrana363@gmail.com>
2023-12-24 18:12:07 +0600

    producSlider model  done

---
+++ src/models/ProductSliderModel.js
@@ -0,0 +1,29 @@
+const  mongoose = require("mongoose");
+
+const dataSchema = new mongoose.Schema({
+    title : {
+        type : String,
+        required : true
+    },
+    des : {
+        type : String,
+        required : true
+    },
+    price : {
+        type : String,
+        required : true
+    },
+    img : {
+        type : String,
+        required : true
+    },
+    productID : {
+        type : mongoose.Schema.Types.ObjectId,
+        required : true
+    },
+},{timestamps:true,versionKey:false});
+
+const productSliderModel = mongoose.model("productSliders",dataSchema);
+
+module.exports = productSliderModel;
+
