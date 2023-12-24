8037cac91f21f598c89dafb974e5a3f51c354d73
ishanrana363 <ishanrana363@gmail.com>
2023-12-24 18:27:51 +0600

    product review model done

---
+++ src/models/ReviewModel.js
@@ -0,0 +1,24 @@
+const  mongoose = require("mongoose");
+
+const dataSchema = new mongoose.Schema({
+    productID : {
+        type : mongoose.Schema.Types.ObjectId,
+        required : true
+    },
+    userID : {
+        type : mongoose.Schema.Types.ObjectId,
+        required : true
+    },
+    des : {
+        type : String,
+        required : true
+    },
+    rating : {
+        type : String,
+        required : true
+    },
+},{timestamps:true,versionKey:false})
+
+const reviewModel = mongoose.model("reviews",dataSchema);
+
+module.exports = reviewModel;
